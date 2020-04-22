55d1d9b95c90b4deca520e5a8671aa282701c94e
jwgdmkj <vldpfh548@gmail.com>
2020-04-22 20:44:46 +0900

    gaji1

---
+++ Fortest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	printf("이건 가지1인가");
+	printf("이건 가지2인가");
+	printf("이건 가지3인가");
+	printf("이건 가지4인가");
+	printf("이건 마스터인가");
+
+	return 0;
+}
